d6acad0c5dd5c7a23d88be9a79552b5b7da27fef
Bhushan <bhushanpawar9001@gmail.com>
2023-05-24 16:58:57 +0530

    mongo connection done

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect("mongodb+srv://mayur:mayur9799@cluster0.emmy41q.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('MongoDB connection error:', error);
+    });
+
 app.get('/',(req,res)=>{
     res.send("<h1>I am Inevitable!</h1>")
 })
